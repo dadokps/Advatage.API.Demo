bcab3832a55e6721d2f29a563b71485cecf33314
Damir <damir.aljovic@msgconsulting.net>
2019-06-01 10:56:37 +0200

    init

---
+++ Advantage.API.Demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using Advantage.API.Demo.Models;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+namespace Advantage.API.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = BuildWebHost(args);
+            host.Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
---
+++ Advantage.API.Demo/Startup.cs
@@ -0,0 +1,60 @@
+﻿using Advantage.API.Demo.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Advantage.API.Demo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(opt =>
+                {
+                    opt.AddPolicy("CorsPolicy",
+                        b => b.AllowAnyOrigin()
+                              .AllowAnyMethod()
+                              .AllowAnyHeader()
+                              .AllowCredentials());
+                }
+            );
+
+            services.AddMvc();
+
+            services.AddEntityFrameworkNpgsql()
+                .AddDbContext<ApiContext>(
+                    opt => opt.UseNpgsql(Configuration.GetConnectionString("AdvantageDemo")));
+
+            services.AddTransient<DataSeed>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, DataSeed seeder)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CorsPolicy");
+
+            var nCustomers = 20;
+            var nOrders = 1000;
+            seeder.SeedData(nCustomers, nOrders);
+
+            app.UseMvc(routes =>
+                routes.MapRoute("default", "api/{controller}/{action}/{id?}")
+            );
+        }
+    }
+}
